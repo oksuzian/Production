2b3472b36b42191195ec7ae15835f5a5c7decbe3
Iuri Oksuzian <oksuzian@mu2egpvm05.fnal.gov>
2025-06-05 18:07:47 -0500

    Cosmetic tweaks

--- Scripts/avg_filesize.sh
+++ Scripts/avg_filesize.sh
@@ -1,1 +1,6 @@
-DATASET="$1"; OUTPUT=$(samweb list-files --summary "dh.dataset $DATASET" 2>/dev/null); FILE_COUNT=$(echo "$OUTPUT" | grep "File count:" | awk "{print \$3}"); TOTAL_SIZE=$(echo "$OUTPUT" | grep "Total size:" | awk "{print \$3}"); echo $((TOTAL_SIZE / FILE_COUNT / 1024 / 1024))
+#!/bin/bash
+DATASET="$1"; 
+OUTPUT=$(samweb list-files --summary "dh.dataset $DATASET" 2>/dev/null); 
+FILE_COUNT=$(echo "$OUTPUT" | grep "File count:" | awk "{print \$3}"); 
+TOTAL_SIZE=$(echo "$OUTPUT" | grep "Total size:" | awk "{print \$3}"); 
+echo $((TOTAL_SIZE / FILE_COUNT / 1024 / 1024))
