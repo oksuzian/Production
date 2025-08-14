5ec3c8d7003b30bb2f6748d39c9fd1728f9b3337
sophieMu2e <sophie@fnal.gov>
2025-08-14 14:31:07 -0500

    updated path

--- JobConfig/ensemble/scripts/livetime_checker.sh
+++ JobConfig/ensemble/scripts/livetime_checker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DTS="onefile.txt"
-TAG="MDS2b"
+DTS="MDS2b.txt" # Edit with file names
+TAG="MDS2b" # Edit as needed
 rm *.livetime
 mu2e -c Offline/Print/fcl/printCosmicLivetime.fcl -S ${DTS} | grep 'Livetime:' | awk -F: '{print $NF}' > ${TAG}.livetime
 LIVETIME=$(awk '{sum += $1} END {print sum}' ${TAG}.livetime)
