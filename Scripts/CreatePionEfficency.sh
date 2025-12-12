9763a54f21221653409fa554d3ce204580c8e3aa
sophieMu2e <sophie@fnal.gov>
2025-12-12 10:35:04 -0600

    sim effs

--- Scripts/CreatePionEfficency.sh
+++ Scripts/CreatePionEfficency.sh
@@ -3,15 +3,21 @@
 # Script to create the SimEfficiency proditions content from a beam campaign.  The campaign 'configuration' field must be provided
 # stopped pion campaign is the argument
 rm $1_SimEff.txt
-mu2eGenFilterEff --out=$1_SimEff.txt --chunksize=100 --firstLine "PionSimEfficiencies" --verbosity 3 sim.mu2e.PiBeamCat.$1.art sim.mu2e.PiTargetStops.$1.art 
+mu2eGenFilterEff --out=$1_SimEff.txt --chunksize=100 --firstLine "PionSimEfficiencies" --verbosity 3 sim.mu2e.IPAStopsCat.$1.art sim.mu2e.PiBeamCat.$1.art sim.mu2e.PiTargetStops.$1.art sim.mu2e.PiMinusFilter.$1.art sim.mu2e.PhysicalPionStops.$1.art
 
-#for  filter version you need the eventCount of sim.mu2e.PiTargetStops.$1.art as the denominator
-PREFILTER=$(samDatasetsSummary.sh sim.mu2e.PiTargetStops.$1.art  | awk '/Triggered/ {print $2}')
-FILTER=$(samDatasetsSummary.sh sim.mu2e.PiTargetFilt.$1.art  | awk '/Triggered/ {print $2}')
-FILTEREFF=$(awk "BEGIN {printf \"%.5f\", ${FILTER}/${PREFILTER}}") 
-echo "PiTargetFilt, ${PREFILTER}, ${FILTER}, ${FILTEREFF}" >> $1_SimEff.txt
+mu2eDatasetFileList sim.mu2e.PiMinusFilter.$1.art > filenames_pionfilter
 
-TOTALWEIGHT=37468 #TODO need better way of adding to the file
-SELECTEDWEIGHT=5.7055 #TODO need better way of adding to the file
-echo "PiTotalLifeimeWeight,,,${TOTALWEIGHT}" >> $1_SimEff.txt
-echo "PiSelectedLifeimeWeight,,,${SELECTEDWEIGHT}" >> $1_SimEff.txt
+TOTALWEIGHT_FILTER=$(getWeights.py --weight "total" --files filenames_pionfilter --tag "filter")
+SELECTEDWEIGHT_FILTER=$(getWeights.py  --weight "selected" --files filenames_pionfilter --tag "filter")
+
+rm filenames_pionfilter
+mu2eDatasetFileList sim.mu2e.PhysicalPionStops.$1.art > filenames_pionfilter
+
+SELECTEDWEIGHT_SAMPLER=$(getWeights.py  --weight "selected" --files filenames_pionfilter --tag "sampler")
+
+rm filenames_pionfilter
+
+echo "PiTotalLifeimeWeight_filter, 0, 0, ${TOTALWEIGHT_FILTER}" >> $1_SimEff.txt
+echo "PiSelectedLifeimeWeight_filter, 0, 0, ${SELECTEDWEIGHT_FILTER}" >> $1_SimEff.txt
+echo "PiSelectedLifeimeWeight_sampler, 0, 0, ${SELECTEDWEIGHT_SAMPLER}" >> $1_SimEff.txt
+
